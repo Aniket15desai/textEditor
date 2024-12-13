72e58538a66f301f16d922babcb44abfe6f3eadf
aniket15desai <aniketsardesai44@gmail.com>
2024-12-13 17:46:55 +0530

    React Sorcerer Take Home Test

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 import React from "react";
 
 interface TitleProps {
-  name: string;
+    name: string;
 }
 
 const Title: React.FC<TitleProps> = ({ name }) => {
-  return <h3>Demo editor by <Link href={`https://github.com/aniket15desai/`} className="underline"><strong><em>{name}</em></strong></Link></h3>;
+    return <h3>Demo editor by <Link href={`https://github.com/aniket15desai/`} className="underline"><strong><em>{name}</em></strong></Link></h3>;
 };
 
 export default Title;
